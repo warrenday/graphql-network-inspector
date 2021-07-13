4cf4e2ed04e26cc32871f4be4572ac3a34698543
Warren Day <warrenjday@gmail.com>
2021-07-13 17:46:34 +0100

    migrating styles

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -18,19 +18,23 @@ export const Tabs = (props: TabsProps) => {
 
   return (
     <div className="h-screen flex flex-col">
-      <div className="flex border-b-2 border-gray-600">
+      <div className="flex border-b border-gray-600">
         {leftContent && leftContent}
-        {tabs.map((tab, i) => (
-          <button
-            key={i}
-            className={cx("px-4 py-2 bg-none", {
-              "bg-gray-700": i === activeTab,
-            })}
-            onClick={() => setActiveTab(i)}
-          >
-            <h2 className="font-bold">{tab.title}</h2>
-          </button>
-        ))}
+        {tabs.map((tab, i) => {
+          const isActive = i === activeTab;
+          return (
+            <button
+              key={i}
+              className={cx("px-4 py-2 bg-none", {
+                "bg-gray-700": isActive,
+                "text-gray-400": !isActive,
+              })}
+              onClick={() => setActiveTab(i)}
+            >
+              <h2 className="font-bold">{tab.title}</h2>
+            </button>
+          );
+        })}
       </div>
       <div className="scroll">{tabs[activeTab].component()}</div>
     </div>
