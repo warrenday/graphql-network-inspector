2c73cac3e04d55bd13faa67d6ff76245056c7b48
Warren Day <warrenjday@gmail.com>
2021-07-22 09:04:52 +0100

    add mock to remove listeners

--- src/mocks/mock-chrome.ts
+++ src/mocks/mock-chrome.ts
@@ -1,5 +1,15 @@
 import { mockRequests } from "../mocks/mock-requests";
 
+let listeners: Array<() => void> = [];
+const removeListeners = () => {
+  while (listeners.length) {
+    const listener = listeners.shift();
+    if (listener) {
+      listener();
+    }
+  }
+};
+
 export const mockChrome = {
   devtools: {
     network: {
@@ -9,16 +19,22 @@ export const mockChrome = {
             cb(mockRequest as any);
           });
 
-          // Test add more requests on space
-          window.addEventListener("keydown", (e) => {
+          // On press key "1", add more mock requests to app
+          const handleKeydown = (e: KeyboardEvent) => {
             if (e.code === "Digit1") {
               mockRequests.forEach((mockRequest) => {
                 cb(mockRequest as any);
               });
             }
+          };
+          window.addEventListener("keydown", handleKeydown);
+          listeners.push(() => {
+            window.removeEventListener("keydown", handleKeydown);
           });
         },
-        removeListener: (cb) => {},
+        removeListener: (cb) => {
+          removeListeners();
+        },
       },
       onNavigated: {
         addListener: (cb) => {},
