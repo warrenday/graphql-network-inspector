ec4d2571b82c9b4c6c22843e592ffd77aace9df5
Warren Day <warrenjday@gmail.com>
2021-08-06 09:29:08 +0100

    allow preserve checkbox to be controlled via enter key

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -23,6 +23,7 @@ export const Checkbox = (props: ICheckboxProps) => {
         type="checkbox"
         checked={checked}
         onChange={() => onChange(!checked)}
+        onKeyPress={(e) => e.key === "Enter" && onChange(!checked)}
         className="dark:bg-gray-900 form-checkbox rounded-md w-5 h-5"
       />
       <span className="pl-3 dark:text-gray-300">{label}</span>
