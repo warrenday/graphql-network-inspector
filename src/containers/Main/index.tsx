8dbcf7c386ae2f507e077d1dab81c268e53b4648
Warren Day <warrenday@192.168.1.191>
2021-08-23 20:10:08 +0100

    create re-usable split pane layout

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import SplitPane from "react-split-pane";
-import { LayoutMain } from "../../components/Layout";
+import { HeadBodyLayout, SplitPaneLayout } from "../../components/Layout";
 import { NetworkTable } from "../NetworkTable";
 import { NetworkPanel } from "../NetworkPanel";
 import { Toolbar } from "../Toolbar";
@@ -46,56 +46,44 @@ export const Main = () => {
     });
   }, [isPreserveLogs, clearWebRequests]);
 
-  const ToolbarComponent = (
-    <Toolbar
-      filterValue={filterValue}
-      onFilterValueChange={setFilterValue}
-      preserveLogs={isPreserveLogs}
-      onPreserveLogsChange={setIsPreserveLogs}
-    />
-  );
-
-  const NetworkTableComponent = (
-    <NetworkTable
-      data={filteredNetworkRequests}
-      selectedRowId={selectedRowId}
-      onRowClick={setSelectedRowId}
-      onRowSelect={setSelectedRowId}
-      showSingleColumn={Boolean(selectedRequest)}
-      onClear={() => {
-        setSelectedRowId(null);
-        clearWebRequests();
-      }}
-    />
-  );
-
-  if (!selectedRequest) {
-    return (
-      <LayoutMain header={ToolbarComponent} body={NetworkTableComponent} />
-    );
-  }
-
   return (
-    <div className="flex flex-col">
-      <LayoutMain
-        header={ToolbarComponent}
-        body={
-          <SplitPane split="vertical" minSize={130} defaultSize={210}>
-            {NetworkTableComponent}
-            <div
-              className="dark:bg-gray-900 border-l border-gray-300 dark:border-gray-600 h-full"
-              style={{ minWidth: 200 }}
-            >
-              <NetworkPanel
-                data={selectedRequest}
-                onClose={() => {
-                  setSelectedRowId(null);
-                }}
-              />
-            </div>
-          </SplitPane>
-        }
-      />
-    </div>
+    <SplitPaneLayout
+      header={
+        <Toolbar
+          filterValue={filterValue}
+          onFilterValueChange={setFilterValue}
+          preserveLogs={isPreserveLogs}
+          onPreserveLogsChange={setIsPreserveLogs}
+        />
+      }
+      leftPane={
+        <NetworkTable
+          data={filteredNetworkRequests}
+          selectedRowId={selectedRowId}
+          onRowClick={setSelectedRowId}
+          onRowSelect={setSelectedRowId}
+          showSingleColumn={Boolean(selectedRequest)}
+          onClear={() => {
+            setSelectedRowId(null);
+            clearWebRequests();
+          }}
+        />
+      }
+      rightPane={
+        selectedRequest && (
+          <div
+            className="dark:bg-gray-900 border-l border-gray-300 dark:border-gray-600 h-full"
+            style={{ minWidth: 200 }}
+          >
+            <NetworkPanel
+              data={selectedRequest}
+              onClose={() => {
+                setSelectedRowId(null);
+              }}
+            />
+          </div>
+        )
+      }
+    />
   );
 };
---
+++ src/containers/SearchPanel/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface ISearchPanelProps {}
+
+export const SearchPanel = (props: ISearchPanelProps) => {
+  return <div></div>;
+};
