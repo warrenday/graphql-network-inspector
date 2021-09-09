72192c9c62336551fac915cc682c7b8c43049128
warrenday <warrenjday@gmail.com>
2021-09-09 18:51:39 +0100

    fix scroll hiding bottom element

--- src/components/Layout/HeadBodyLayout.tsx
+++ src/components/Layout/HeadBodyLayout.tsx
@@ -8,9 +8,9 @@ interface IHeadBodyLayoutProps {
 export const HeadBodyLayout = (props: IHeadBodyLayoutProps) => {
   const { header, body } = props;
   return (
-    <div className="h-screen flex flex-col overflow-hidden">
+    <div className="h-screen flex flex-col items-stretch overflow-hidden">
       <div>{header}</div>
-      <div className="flex-1 h-full relative">{body}</div>
+      <div className="flex-1 relative overflow-auto">{body}</div>
     </div>
   );
 };
