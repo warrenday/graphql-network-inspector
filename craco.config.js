bf9d2c9a6236595ef68f4a49c0da62752edf516f
Warren Day <warrenday@192.168.1.104>
2021-09-13 16:44:06 +0100

    custom config to allow building to disk during development

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,26 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
+  },
+  webpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "public",
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
+          },
+        ],
+      }),
+    ],
+  },
+  devServer: {
+    writeToDisk: true,
   },
 };
