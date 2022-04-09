0a8ac48f743120e6977920129ebf393e8f2f8a73
Warren Day <warrenjday@gmail.com>
2022-04-09 16:54:42 +0100

    fix lint warning

--- src/hooks/useMark.test.tsx
+++ src/hooks/useMark.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import { useMark } from "./useMark"
 
 const TestComponent = (props: { search: string }) => {
