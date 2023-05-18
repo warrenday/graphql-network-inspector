b69eff86621d6cbb99bce0571a1a95d96151a4aa
Warren Day <warrenday@192.168.1.108>
2023-05-18 17:54:14 +0100

    new scripts to correctly build manifest or chrome and firefox

--- scripts/set-browser-specific-settings.js
+++ scripts/set-browser-specific-settings.js
@@ -1,16 +1,13 @@
 const fs = require("fs")
-const packageJson = require("../package.json")
 
 const manifestPath = "./build/manifest.json"
 
-// "browser_specific_settings": {
-//   "gecko": {
-//     "id": "warrenjday@graphqlnetworkinspector.com"
-//   }
-// }
-
 fs.readFile(manifestPath, "utf-8", (err, json) => {
   const manifest = JSON.parse(json)
-  manifest.version = packageJson.version
+  manifest.browser_specific_settings = {
+    gecko: {
+      id: "warrenjday@graphqlnetworkinspector.com",
+    },
+  }
   fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2), () => {})
 })
