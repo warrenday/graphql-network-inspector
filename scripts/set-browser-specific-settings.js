a87d165d07791fd1986386f28a1b7140136c02ce
Warren Day <warrenday@Warrens-Air.lan>
2023-07-09 17:29:29 +0200

    set share url in build

--- scripts/set-browser-specific-settings.js
+++ scripts/set-browser-specific-settings.js
@@ -1,13 +1,14 @@
 const fs = require("fs")
-
 const manifestPath = "./build/manifest.json"
 
-fs.readFile(manifestPath, "utf-8", (err, json) => {
+const run = async () => {
+  const json = await fs.promises.readFile(manifestPath, "utf-8")
   const manifest = JSON.parse(json)
   manifest.browser_specific_settings = {
     gecko: {
       id: "warrenjday@graphqlnetworkinspector.com",
     },
   }
-  fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2), () => {})
-})
+  await fs.promises.writeFile(manifestPath, JSON.stringify(manifest, null, 2))
+}
+run()
--- scripts/set-manifest-content-script.js
+++ scripts/set-manifest-content-script.js
@@ -2,8 +2,12 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
 const fs = require("fs")
 const manifestPath = "./build/manifest.json"
 
-fs.readFile(manifestPath, "utf-8", (err, json) => {
+const run = async () => {
+  const json = await fs.promises.readFile(manifestPath, "utf-8")
   const manifest = JSON.parse(json)
-  manifest.content_scripts.matches = ["https://www.graphdev.app/draft?*"]
-  fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2), () => {})
-})
+  manifest.content_scripts[0].matches = [
+    `${process.env.REACT_APP_SHARE_TARGET_URL}/draft?*`,
+  ]
+  await fs.promises.writeFile(manifestPath, JSON.stringify(manifest, null, 2))
+}
+run()
