e30f55be0c060994492926f10632b43a6bd4dbf0
Warren Day <warrenday@Warrens-MacBook-Pro.local>
2024-01-15 09:09:39 +0000

    add new event listeners to get request starting

--- src/services/networkMonitor.ts
+++ src/services/networkMonitor.ts
@@ -9,6 +9,34 @@ export const getHAR = async (): Promise<chrome.devtools.network.HARLog> => {
   })
 }
 
+export const onBeforeRequest = (
+  cb: (e: chrome.webRequest.WebRequestBodyDetails) => void
+) => {
+  const chrome = chromeProvider()
+
+  chrome.webRequest.onBeforeRequest.addListener(cb, { urls: ["<all_urls>"] }, [
+    "requestBody",
+  ])
+  return () => {
+    chrome.webRequest.onBeforeRequest.removeListener(cb)
+  }
+}
+
+export const onBeforeSendHeaders = (
+  cb: (e: chrome.webRequest.WebRequestHeadersDetails) => void
+) => {
+  const chrome = chromeProvider()
+
+  chrome.webRequest.onBeforeSendHeaders.addListener(
+    cb,
+    { urls: ["<all_urls>"] },
+    ["requestHeaders"]
+  )
+  return () => {
+    chrome.webRequest.onBeforeSendHeaders.removeListener(cb)
+  }
+}
+
 export const onRequestFinished = (
   cb: (e: chrome.devtools.network.Request) => void
 ) => {
@@ -27,3 +55,7 @@ export const onNavigate = (cb: () => void) => {
     chrome.devtools.network.onNavigated.removeListener(cb)
   }
 }
+
+// Can I get request body in webrequest event?
+// If so, can just match on headers, method, body.
+// Just associate first response where match, is found.
